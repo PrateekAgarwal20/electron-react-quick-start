7eabd12fcd89f16aeee3686b6e66f7434157555c
git status <spike.lu@emory.edu>
2017-07-27 11:01:39 -0700

    hi

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ passport.deserializeUser(function(id, done) {
 
 // passport strategy
 passport.use(new LocalStrategy(function(username, password, done) {
-    console.log("LocalStrategy");
+  console.log("LocalStrategy");
   // Find the user with the given username
   models.User.findOne({ username: username }, function (err, user) {
     // if there's an error, finish trying to authenticate (auth failed)
